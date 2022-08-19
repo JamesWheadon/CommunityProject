ae0a3ccde0c53a732aa22d9f307ca43ca324f0b7
natbibi <natalie.wuwu@googlemail.com>
2022-08-19 18:02:50 +0100

    :clown_face: add fakedata and render map markers with basic animal info when clicked

--- react-client/src/components/map.js
+++ react-client/src/components/map.js
@@ -1,5 +1,6 @@
-import { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { useMemo, useState } from "react";
+import { GoogleMap, useLoadScript, MarkerF, InfoWindow } from "@react-google-maps/api";
+import mapStyles from "../mapStyles";  
 
 export const Map = () => {
   const { isLoaded } = useLoadScript({
@@ -11,11 +12,69 @@ export const Map = () => {
 }
 
 const FetchMap = () => {
-  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+  const center = useMemo(() => ({ lat: 51.507351, lng: -0.127758 }), []);
+  const [selectedItem, setSelectedItem] = useState(null);
+  
+  const fakeData = [
+    {
+        animal_id: 1,
+        animal_name: 'Addax',
+        animal_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/A_big_male_Addax_showing_as_the_power_of_his_horns.jpg/800px-A_big_male_Addax_showing_as_the_power_of_his_horns.jpg?20160529142107',
+        latitude: 31.20,
+        longitude: -6.35
+    },
+    {
+      animal_id: 2,
+      animal_name: 'African_bush_elephant',
+      animal_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/400px-African_Bush_Elephant.jpg',
+      latitude: -32.44,
+      longitude: 24.74
+    },
+    {
+      animal_id: 3,
+      animal_name: 'African_wild_dog',
+      animal_image: 'https://commons.wikimedia.org/wiki/File:African_wild_dog_(Lycaon_pictus_pictus).jpg',
+      latitude: -18.84,
+      longitude: 34.44
+    },
+  ]
 
   return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      <Marker position={center} />
+    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container" options={{ styles: mapStyles.styles }}>
+      {fakeData.map(data => (
+        <MarkerF 
+          key={data.animal_id}
+          position={{
+            lat: data.latitude,
+            lng: data.longitude
+          }} 
+          onClick={() => {
+            setSelectedItem(data);
+          }}
+          icon={{
+            url: 'https://w7.pngwing.com/pngs/458/589/png-transparent-pig-emoji-rosa-face-animal-cute-emoticon-happy-excited-alegre.png',
+            scaledSize: new window.google.maps.Size(75, 75)
+          }}
+        />
+      ))}
+
+      {selectedItem && (
+        <InfoWindow
+          position={{
+            lat: selectedItem.latitude,
+            lng: selectedItem.longitude
+          }} 
+          onCloseClick={() => {
+            setSelectedItem(null);
+          }}>
+            <div>
+              <h1>{selectedItem.animal_name}</h1>
+              <img src={selectedItem.animal_image} alt={selectedItem.animal_name} height="300px" width="300px"/>
+            </div>
+            </InfoWindow>
+      )
+
+      }
     </GoogleMap>
   );
 }
